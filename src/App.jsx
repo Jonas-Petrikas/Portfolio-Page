c3d4ac7a8617037104b65ef26b21f89b689ee95b
Jonas <jonas@jonaspetrikas.com>
2025-07-09 16:58:03 +0300

    Added footer and a beginning of portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
-import Header from './components/Header'
-import Hero from './components/Hero'
+import './App.scss';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
       <Hero />
+      <Portfolio/>
+      <Footer/>
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,27 @@
-import './style/Hero.scss';
-import codeBg from '../assets/images/code-bg.webp'
-import heroPortrait from '../assets/images/Jonas.webp'
+import "./style/Hero.scss";
+import heroPortrait from "../assets/images/Jonas.webp";
 
 export default function Hero() {
-    return (
-        <>
-            {/* <img className='hero-bg' src={codeBg} alt="background picture with code lines" /> */}
-            <div className="hero-container wrapper" >
-
-
-                <div className="left">
-                    <h1>Jonas Petrikas - Website development</h1>
-                    <p>hellow</p>
-                    <div className="hero-btns">
-                        <button>Get in touch!</button>
-                        <button>Learn more</button>
-
-                    </div>
-
-                </div>
-                <div className="right">
-                    <img className='hero-portrait' src={heroPortrait} alt="Jonas Petrikas portrait" />
-
-
-                </div>
-            </div>
-        </>
-
-    )
-
+  return (
+    <>
+      {/* <img className='hero-bg' src={codeBg} alt="background picture with code lines" /> */}
+      <div className="hero-container wrapper">
+        <div className="left">
+          <h1>Web Developer</h1>
+          <p>Custom web solutions that grow your business</p>
+          <div className="hero-btns">
+            <button>View My Work</button>
+            <button>Get in touch!</button>
+          </div>
+        </div>
+        <div className="right">
+          <img
+            className="hero-portrait"
+            src={heroPortrait}
+            alt="Jonas Petrikas portrait"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
