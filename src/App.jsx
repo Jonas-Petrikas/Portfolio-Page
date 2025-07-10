4ce87a876c8534e837ba1bd1d45c7891c13e526c
Jonas <jonas@jonaspetrikas.com>
2025-07-10 22:41:38 +0300

    deploy?

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.scss';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Portfolio/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
