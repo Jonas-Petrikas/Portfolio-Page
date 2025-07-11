e9eb411c3c77b85e67f83ba9dab511a1d7d67793
Jonas <jonas@jonaspetrikas.com>
2025-07-11 14:32:17 +0300

    smtp update

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -4,7 +4,7 @@ export default function Contacts() {
     return (
         <section className='contacts' id='contacts'>
             <h2>Contacts</h2>
-            <form action="../send.php" method="POST">
+            <form action="https://dev.jonaspetrikas.com/send.php" method="POST">
                 <input type="text" name="name" placeholder="Your name" required />
                 <input type="email" name="email" placeholder="Your email" required />
                 <textarea name="message" placeholder="Your message" required></textarea>
